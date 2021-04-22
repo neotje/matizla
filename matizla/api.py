567dc1e8e9a8de853d0ef8db6a937a85e131b844
neotje <neotje111@gmail.com>
2021-04-22 23:10:52 +0200

    window wrappers

--- matizla/api.py
+++ matizla/api.py
@@ -7,24 +7,31 @@ from matizla import ui
 def get_by_uuid_window(uuid: str) -> Window:
     for w in webview.windows:
         if w.uuid == uuid:
-            return Window
+            return w
 
+def get_window_by_title(title: str) -> Window:
+    for w in webview.windows:
+        if w.title == title:
+            return w
 
 class MatizlaWebApi:
     def __init__(self) -> None:
         pass
 
-    def showWindow(self, uuid: str):
-        w = get_by_uuid_window(uuid)
+    def showWindow(self, title: str):
+        w = get_window_by_title(title)
 
         if w is not None:
             w.show()
 
-    def hideWindow(self, uuid: str):
-        w = get_by_uuid_window(uuid)
+    def hideWindow(self, title: str):
+        w = get_window_by_title(title)
 
         if w is not None:
             w.hide()
+
+    def fadeInMainWindow(self):
+        ui.MainScreen.fade_in()
 
     def windowList(self) -> List:
         r = []
--- matizla/ui.py
+++ matizla/ui.py
@@ -13,14 +13,10 @@ from matizla.assets import MainScreen
 main_window_server = Flask(
     __name__,
     static_folder=MainScreen.__path__[0],
-    template_folder=MainScreen.__path__[0]
+    template_folder=MainScreen.__path__[0],
+    static_url_path=''
 )
 main_window_server.config["SEND_FILE_MAX_AGE_DEFAULT"] = 1
-
-
-@main_window_server.route('/')
-def loadingIndex():
-    return render_template('index.html')
 
 
 """ Loading window flask stuff """
@@ -35,8 +31,10 @@ loading_window_server.config["SEND_FILE_MAX_AGE_DEFAULT"] = 1
 
 
 @loading_window_server.route('/')
+@main_window_server.route('/')
 def loadingIndex():
     return render_template('index.html')
+
 
 @main_window_server.after_request
 @loading_window_server.after_request
@@ -44,26 +42,102 @@ def add_header(response):
     response.headers['Cache-Control'] = 'no-store'
     return response
 
-MAIN_WINDOW: Window = webview.create_window(
-    "main",
-    main_window_server,
-    width=800,
-    height=480,
-    resizable=False,
-    fullscreen=(platform.machine() == "armv7l"),
-    on_top=False,
-    js_api=MatizlaWebApi()
-)
-LOAD_WINDOW: Window = webview.create_window(
-    "main",
-    loading_window_server,
-    width=800,
-    height=480,
-    resizable=False,
-    fullscreen=(platform.machine() == "armv7l"),
-    on_top=True,
-    transparent=False
-)
+
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store'
+    return response
+
+
+_api = MatizlaWebApi()
+
+
+class MainScreen:
+    WINDOW: Window = webview.create_window(
+        "main",
+        main_window_server,
+        width=800,
+        height=480,
+        resizable=False,
+        fullscreen=(platform.machine() == "armv7l"),
+        on_top=False,
+        js_api=_api
+    )
+
+    @staticmethod
+    def hide():
+        MainScreen.WINDOW.hide()
+
+    @staticmethod
+    def show():
+        MainScreen.WINDOW.show()
+
+    @staticmethod
+    def fade_out():
+        MainScreen.WINDOW.evaluate_js("fadeOut();")
+
+    @staticmethod
+    def fade_in():
+        MainScreen.WINDOW.evaluate_js("fadeIn();")
+        MainScreen.show()
+
+
+class LoadingScreen:
+    WINDOW: Window = webview.create_window(
+        "load",
+        loading_window_server,
+        width=800,
+        height=480,
+        resizable=False,
+        fullscreen=(platform.machine() == "armv7l"),
+        on_top=True,
+        transparent=False,
+        js_api=_api
+    )
+
+    @staticmethod
+    def show_progres_bar():
+        LoadingScreen.WINDOW.evaluate_js("showProgressBar();")
+
+    @staticmethod
+    def hide_progress_bar():
+        LoadingScreen.WINDOW.evaluate_js("hideProgressBar();")
+
+    @staticmethod
+    def set_progress(p: float):
+        LoadingScreen.WINDOW.evaluate_js(f"progress = {p};")
+
+    @staticmethod
+    def get_progress() -> float:
+        return float(LoadingScreen.WINDOW.evaluate_js("progress;"))
+
+    @staticmethod
+    def hide():
+        LoadingScreen.WINDOW.hide()
+
+    @staticmethod
+    def show():
+        LoadingScreen.WINDOW.show()
+
+    @staticmethod
+    def fade_out():
+        LoadingScreen.WINDOW.evaluate_js("fadeOut();")
+
+    @staticmethod
+    def fade_in():
+        LoadingScreen.WINDOW.evaluate_js("fadeIn();")
+        LoadingScreen.show()
+
+
+def switchToMain():
+    LoadingScreen.fade_out()
+    sleep(0.95)
+    MainScreen.fade_in()
+
+
+def switchToLoad():
+    MainScreen.fade_out()
+    sleep(0.95)
+    LoadingScreen.fade_in()
 
 
 def close():
@@ -72,7 +146,7 @@ def close():
 
 
 def start(func):
-    MAIN_WINDOW.closed += close
-    LOAD_WINDOW.closed += close
+    MainScreen.WINDOW.closed += close
+    LoadingScreen.WINDOW.closed += close
 
     webview.start(debug=True, func=func)
